4a37a8e768371a4bf7c9c934750e6a69233ba444
kodhus <kodhus@kodhus.com>
2019-10-11 17:32:00 +0200

    some-minor-text-improvements

--- src/components/Colors.stories.js
+++ src/components/Colors.stories.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import { storiesOf } from "@storybook/react";
 
 export default {
-  title: "Extra | Styles",
+  title: "Global | Styles",
   parameters: {
     componentSubtitle: "Design system colors"
   }
